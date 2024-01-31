8717c37b0c16c2fefca3af6a555aeab3f096db27
Arno <80052680+abrsc@users.noreply.github.com>
2024-01-31 12:32:19 +0100

    Create bateria 1-1.py

---
+++ bateria 1-1.py
@@ -0,0 +1,5 @@
+# Programa que pregunta dos números a l'usuari i calcula la seva suma.
+numero1 = input("Si us plau entreu el primer número necessari per la suma: ")
+numero2 = input("Si us plau entreu el segon número necessari per la suma: ")
+resultat = float(numero1) + float(numero2)
+print(numero1, "+", numero2, "=", resultat)
