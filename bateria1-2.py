cdc620e10a39479a1604502f020b55b4cf84e275
Arno <80052680+abrsc@users.noreply.github.com>
2024-01-31 12:32:51 +0100

    Create bateria1-2.py

---
+++ bateria1-2.py
@@ -0,0 +1,4 @@
+# Programa que pregunta un número i retorna el seu quadrat.
+numero1 = input("Si us plau entreu un número per calcular el seu quadrat: ")
+resultat = float(numero1) ** 2
+print("El cuadrat de", numero1, "és de", resultat)
