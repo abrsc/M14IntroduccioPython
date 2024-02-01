886c59dfb55aecef85b2eb17b1a22d6061f32188
Arno <80052680+abrsc@users.noreply.github.com>
2024-02-01 14:04:58 +0100

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,9 @@
+# Càlcul de numèro factorial amb break
+num = int(input("Insereu un nùmero per saber el seu factorial si us plau: "))
+resultat = 1
+while True:
+    resultat = resultat * num
+    num = num - 1
+    if num == 0:
+        break
+print("El factorial és", resultat)
