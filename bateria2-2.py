d461c4271472f425facdc4c61a839b7578b678d2
Arno <80052680+abrsc@users.noreply.github.com>
2024-02-02 13:28:49 +0100

    Create bateria2-2.py

---
+++ bateria2-2.py
@@ -0,0 +1,8 @@
+# Programa que pregunta un número a l'usuari i diu si és major o menor que 100.
+num = float(input("Insereu un nùmero si us plau: "))
+if num > 100:
+  print("El número que has triat és superior a 100.")
+elif num == 100:
+  print("El número que has triat és igual a 100.")
+else:
+  print("El número que has triat és inferior a 100.")
