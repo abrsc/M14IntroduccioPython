258bfc3bf2371574ccde6c57d19cc471eb311537
Arno <80052680+abrsc@users.noreply.github.com>
2024-02-02 13:39:51 +0100

    Update bateria2-3.py

--- bateria2-3.py
+++ bateria2-3.py
@@ -1,5 +1,5 @@
 # Programa que pregunta un número a l'usuari i diu si és senar o parell
-num = float(input("Insereu un número si us plau: "))
+num = int(input("Insereu un número si us plau: "))
 resultat = num % 2
 if resultat == 1:
   print("El número és senar.")
