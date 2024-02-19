6466a1d365375d3329e03650145351ca1c316036
Arno <80052680+abrsc@users.noreply.github.com>
2024-02-19 13:59:34 +0100

    Update pycalc.py

--- pycalc.py
+++ pycalc.py
@@ -19,11 +19,11 @@ while True:
     elif seleccio == "2":
         numero1 = float(input(Fore.LIGHTGREEN_EX + "Introdueix el primer número: "))
         numero2 = float(input("Introdueix el segon número: "))
-        print("La suma de", numero1, "-", numero2, "és: ", numero1 - numero2)
+        print("La resta de", numero1, "-", numero2, "és: ", numero1 - numero2)
     elif seleccio == "3":
         numero1 = float(input(Fore.MAGENTA + "Introdueix el primer número: "))
         numero2 = float(input("Introdueix el segon número: "))
-        print("La suma de", numero1, "*", numero2, "és: ", numero1 * numero2)
+        print("La multiplicació de", numero1, "*", numero2, "és: ", numero1 * numero2)
     elif seleccio == "4":
         while True: #Permet de tornar a elegir números si el usuari decide de dividir per 0.
             numero1 = float(input(Fore.YELLOW + "Introdueix el primer número: ")) 
@@ -31,7 +31,7 @@ while True:
             if numero2 == 0: #Comprova que l'usuari no intenta de dividir per 0.
                 print(Fore.RED + "No es pot dividir per 0!")
             else:
-                print("La suma de", numero1, "/", numero2, "és: ", numero1 / numero2)
+                print("La divisió de", numero1, "/", numero2, "és: ", numero1 / numero2)
                 break #Permet de tornar al primer bucle.
     else:
         print(Fore.RED + "Assegureu-vos que heu escrit un número vàlid.") #Codi d'error si el usuari sel·lecionna un número invalid.
