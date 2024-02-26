a040aab2fe8cb50fe5257deb153518a6b0652f90
Arno <80052680+abrsc@users.noreply.github.com>
2024-02-26 12:22:00 +0000

    update

--- bateria3-5.py
+++ bateria3-5.py
@@ -1,1 +1,10 @@
 # Fes un programa que imprimeixi per pantalla els números primers.
+num = 1
+i = 2
+while i < num and num % i != 0:
+    i += 1
+    if i == num:
+        print(num)
+        num += 1
+    else:
+        num += 1
