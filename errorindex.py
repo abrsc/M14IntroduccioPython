214f59b08d26efd86a5ae2598ff20982553b97de
Arno <80052680+abrsc@users.noreply.github.com>
2024-02-26 12:42:56 +0000

    modificar deures

--- errorindex.py
+++ errorindex.py
@@ -1,3 +1,4 @@
+# Crea una llista amb alguns elements i demana a l'usuari un índex. Utilitza try i except per gestionar l'error que es produeix quan l'índex no existeix a la llista.
 llista = [1,5,15,78,456,87]
 preguntaindex = input("Entreu un número enter per ver a que correspon en la llista: ")
 try:
