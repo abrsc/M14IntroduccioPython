aca07aa3c83a7bd07763196557c8ba9051032860
Arno <80052680+abrsc@users.noreply.github.com>
2024-02-29 13:04:46 +0000

    update deberes

---
+++ asterisc.py
@@ -0,0 +1,35 @@
+pos_x = 0
+pos_y = 0
+AMPLADA = 20
+cadena_buida = "|" + " " * AMPLADA + "|"
+cadena_vora = "-" * (AMPLADA+2)
+while True:
+    #genero cadena de text amb l'asterisc a la seva posició:
+    cadena_asterisc = "|"
+    for i in range(AMPLADA):
+        if pos_x == i:
+            cadena_asterisc += "*"
+        else:
+            cadena_asterisc += " "
+    cadena_asterisc += "|"
+    #imprimerixo l'asterisc:
+    print(cadena_vora)
+    for e in range(AMPLADA+1):
+        if pos_y == e:
+            print(cadena_asterisc)
+        else:
+            print(cadena_buida)
+    print(cadena_vora)
+    #demano el moviment al user:
+    moviment = input("")
+
+    #aplico el moviment de l'usuari:
+    if moviment == 'a' and pos_x > 0:
+            pos_x -= 1
+    elif moviment == 'd' and pos_x < AMPLADA-1:
+            pos_x += 1
+    elif moviment == 'w' and pos_y > 0:
+            pos_y -= 1
+    elif moviment == 's' and pos_y < AMPLADA:
+            pos_y += 1
+
