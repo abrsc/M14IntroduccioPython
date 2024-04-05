fba4f908becd162bb463d9aca4f7b80980fae767
abrsc <80052680+abrsc@users.noreply.github.com>
2024-04-05 13:56:28 +0200

    fer un menu funcional

--- Pygame/menu.py
+++ Pygame/menu.py
@@ -1,9 +1,33 @@
-import pygame, time
+import time
+from pygame.locals import *
+import pygame
 
 AMPLADA = 320
 ALTURA = 200
 BACKGROUND_IMAGE = 'Assets/TitleScreen.png'
 WHITE = (255,255,255)
+running = True
+partida = False
+
+# Jugador 1:
+player_image = pygame.image.load('assets/Nau.png')
+player_rect = player_image.get_rect(midbottom=(AMPLADA // 2, ALTURA - 10))
+velocitat_nau = 2
+
+# Jugador 2:
+player_image2 = pygame.image.load('assets/Nau2.png')
+player_rect2 = player_image2.get_rect(midbottom=(AMPLADA // 2, ALTURA - 150))
+velocitat_nau2 = 2
+
+# Bala rectangular blanca:
+bala_imatge = pygame.Surface((4,10)) #definim una superficie rectangle de 4 pixels d'ample i 10 d'alçada
+bala_imatge.fill(WHITE) #pintem la superficie de color blanc
+bales_jugador1 = [] #llista on guardem les bales del jugador 1
+bales_jugador2 = [] #llista on guardem les bales del jugador 2
+velocitat_bales = 3
+temps_entre_bales = 1000 #1 segon
+temps_ultima_bala_jugador1 = 0 #per contar el temps que ha passat des de que ha disparat el jugador 1
+temps_ultima_bala_jugador2 = 0 #per contar el temps que ha passat des de que ha disparat el jugador 2
 
 pygame.init()
 #pygame.mixer.init()
@@ -11,11 +35,20 @@ pygame.init()
 #pygame.mixer.music.play(-1, 12, 3000)
 #pygame.mixer.music.set_volume(0.75)
 pantalla = pygame.display.set_mode((AMPLADA, ALTURA))
-pygame.display.set_caption("Arcade")
+pygame.display.set_caption("Vaseleyo")
 background = pygame.image.load(BACKGROUND_IMAGE).convert()
 
 # CREAR LA SUPERFÍCIE TRANSPARENT I EL RECTANGLE SOBRE ELLA:
 seccio_transparent = pygame.Surface((240,120),pygame.SRCALPHA)
+
+# Control de FPS
+clock = pygame.time.Clock()
+fps = 30
+
+def imprimir_pantalla_fons(image):
+    # Imprimeixo imatge de fons:
+    background = pygame.image.load(image).convert()
+    pantalla.blit(background, (0, 0))
 
 def TextPantalla(pantalla, font, tamany, text, color, posicio):
     font = pygame.font.SysFont(font,tamany)
@@ -47,9 +80,101 @@ def menuprincipal():
     TextPantalla(pantalla, None, 24, "1.- Crèdits", WHITE, (50,80))
     TextPantalla(pantalla, None, 24, "2.- Jugar", WHITE,(50,100))
     TextPantalla(pantalla,None, 24, "3.- Sortir", WHITE, (50,120))
+    pygame.display.update()
 
-while True:
+#animacioinici()
+menuprincipal()
+while running:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        if event.type == KEYDOWN:
+            if event.key == K_2:
+                partida = True
+            if event.key == K_3:
+                running = False
+
+    if partida == True:
+       BACKGROUND_IMAGE = 'Assets/fondo.png'
+       while True:
+    #contador
+        current_time = pygame.time.get_ticks()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            # controlar trets de les naus
+            if event.type == KEYDOWN:
+                #jugador 1
+                if event.key == K_w and current_time - temps_ultima_bala_jugador1 >= temps_entre_bales:
+                    bales_jugador1.append(pygame.Rect(player_rect.centerx - 2, player_rect.top, 4, 10))
+                    temps_ultima_bala_jugador1 = current_time
+                # jugador 2
+                if event.key == K_UP and current_time - temps_ultima_bala_jugador2 >= temps_entre_bales:
+                    bales_jugador2.append(pygame.Rect(player_rect2.centerx - 2, player_rect2.bottom -10, 4, 10))
+                    temps_ultima_bala_jugador2 = current_time
+
+        # Moviment del jugador 1
+        keys = pygame.key.get_pressed()
+        if keys[K_a]:
+            player_rect.x -= velocitat_nau
+        if keys[K_d]:
+            player_rect.x += velocitat_nau
+        # Moviment del jugador 2
+        if keys[K_LEFT]:
+            player_rect2.x -= velocitat_nau2
+        if keys[K_RIGHT]:
+            player_rect2.x += velocitat_nau2
+
+
+
+        # Mantenir al jugador dins de la pantalla:
+        player_rect.clamp_ip(pantalla.get_rect())
+        player_rect2.clamp_ip(pantalla.get_rect())
+
+        #dibuixar el fons:
+        imprimir_pantalla_fons(BACKGROUND_IMAGE)
+
+        #Actualitzar i dibuixar les bales del jugador 1:
+        for bala in bales_jugador1: # bucle que recorre totes les bales
+            bala.y -= velocitat_bales # mou la bala
+            if bala.bottom < 0 or bala.top > ALTURA: # comprova que no ha sortit de la pantalla
+                bales_jugador1.remove(bala) # si ha sortit elimina la bala
+            else:
+                pantalla.blit(bala_imatge, bala) # si no ha sortit la dibuixa
+            # Detectar col·lisions jugador 2:
+            if player_rect2.colliderect(bala):  # si una bala toca al jugador1 (el seu rectangle)
+                print("BOOM 1!")
+                bales_jugador1.remove(bala)  # eliminem la bala
+                # mostrem una explosió
+                # eliminem el jugador 1 (un temps)
+                # anotem punts al jugador 1
+
+        # Actualitzar i dibuixar les bales del jugador 2:
+        for bala in bales_jugador2:
+            bala.y += velocitat_bales
+            if bala.bottom < 0 or bala.top > ALTURA:
+                bales_jugador2.remove(bala)
+            else:
+                pantalla.blit(bala_imatge, bala)
+            # Detectar col·lisions jugador 1:
+            if player_rect.colliderect(bala):  # si una bala toca al jugador1 (el seu rectangle)
+                print("BOOM 2!")
+                bales_jugador2.remove(bala)  # eliminem la bala
+                # mostrem una explosió
+                # eliminem el jugador 1 (un temps)
+                # anotem punts al jugador 1
+
+        #dibuixar els jugadors:
+        pantalla.blit(player_image, player_rect)
+        pantalla.blit(player_image2, player_rect2)
+
+        pygame.display.update()
+        clock.tick(fps)
+
+    
+            
+
+
 
